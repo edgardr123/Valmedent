105083b01731a25c56f78df6f1a61bbe5b90c61b
EdgarDuranR <eduranrivadeneyra@gmail.com>
2023-08-21 11:02:06 -0600

    Base

---
+++ src/main/java/com/edgar/valmedent/ValmedentApplication.java
@@ -0,0 +1,13 @@
+package com.edgar.valmedent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValmedentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValmedentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edgar/valmedent/ValmedentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgar.valmedent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValmedentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
